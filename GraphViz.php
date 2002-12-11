ddf9da25b7342632af62dc675a0843a01b25972a
Sebastian Bergmann <sebastian@php.net>
2002-12-11 17:14:18 +0000

    Bump year.
    git-svn-id: http://svn.php.net/repository/pear/packages/Image_GraphViz/trunk@107671 c90b9560-bf6c-de11-be94-00142212c4b1

--- GraphViz.php
+++ GraphViz.php
@@ -1,17 +1,17 @@
 <?php
 //
-// +----------------------------------------------------------------------+
-// | PEAR :: Image :: GraphViz                                            |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2002 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
-// |                    Dr. Volker Göbbels <vmg@arachnion.de>.            |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.00 of the PHP License,      |
-// | that is available at http://www.php.net/license/3_0.txt.             |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
+// +---------------------------------------------------------------------------+
+// | PEAR :: Image :: GraphViz                                                 |
+// +---------------------------------------------------------------------------+
+// | Copyright (c) 2002-2003 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
+// |                    Dr. Volker Göbbels <vmg@arachnion.de>.                 |
+// +---------------------------------------------------------------------------+
+// | This source file is subject to version 3.00 of the PHP License,           |
+// | that is available at http://www.php.net/license/3_0.txt.                  |
+// | If you did not receive a copy of the PHP license and are unable to        |
+// | obtain it through the world-wide-web, please send a note to               |
+// | license@php.net so we can mail you a copy immediately.                    |
+// +---------------------------------------------------------------------------+
 //
 // $Id$
 //
