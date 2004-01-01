210b9111a92d939b78d46acac23332caa02b7e63
Sebastian Bergmann <sebastian@php.net>
2004-01-01 10:31:40 +0000

    Happy New Year!
    git-svn-id: http://svn.php.net/repository/pear/packages/Image_GraphViz/trunk@147612 c90b9560-bf6c-de11-be94-00142212c4b1

--- GraphViz.php
+++ GraphViz.php
@@ -3,7 +3,7 @@
 // +---------------------------------------------------------------------------+
 // | PEAR :: Image :: GraphViz                                                 |
 // +---------------------------------------------------------------------------+
-// | Copyright (c) 2002-2003 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
+// | Copyright (c) 2002-2004 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
 // |                         Dr. Volker Göbbels <vmg@arachnion.de>.            |
 // +---------------------------------------------------------------------------+
 // | This source file is subject to version 3.00 of the PHP License,           |
